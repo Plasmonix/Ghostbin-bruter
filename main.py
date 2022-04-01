c10f0679b4804a8fe28f89691361408471f12395
Plαsmonix <100349657+Plasmonix@users.noreply.github.com>
2022-04-01 13:16:54 +0530

    🐛Bug fixes

--- main.py
+++ main.py
@@ -20,23 +20,26 @@ def UpdateTitle():
 
 def Bruter():
     global valid, invalid
-    code = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(5))
-    headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0","Referer":"https://ghostbin.com/","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate, br","Content-Type":"application/x-www-form-urlencoded"}
-    url = f'https://ghostbin.com/paste/{code}'
-    r = requests.get(url, headers=headers)
-
-    if (r.status_code == 200):
-        lock.acquire()
-        print(' [\u001b[32mVALID\u001b[37m] ' +url)
-        f = open("hits.txt", "a+").write(url+"\n")
-        f.close()
-        valid+=1
-        lock.release()
-    else:
-        lock.acquire()
-        print(' [\u001b[31mINVALID\u001b[37m] '+url)
-        invalid+=1
-        lock.release()
+    try:
+        code = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(5))
+        headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0","Referer":"https://ghostbin.com/","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate, br","Content-Type":"application/x-www-form-urlencoded"}
+        url = f'https://ghostbin.com/paste/{code}'
+        r = requests.get(url, headers=headers)
+    
+        if (r.status_code == 200):
+            lock.acquire()
+            print(' [\u001b[32mVALID\u001b[37m] ' +url)
+            f = open("hits.txt", "a+").write(url+"\n")
+            f.close()
+            valid+=1
+            lock.release()
+        else:
+            lock.acquire()
+            print(' [\u001b[31mINVALID\u001b[37m] '+url)
+            invalid+=1
+            lock.release()
+    except:
+        print(' [\u001b[31mERROR\u001b[37m] Failed to establish connection.')
 
 if __name__ == "__main__":
     Banner()
